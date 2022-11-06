801b0f8f30081663fc1c0c94b1584fb658233267
Levente Farkas <levente.f@gmail.com>
2022-11-06 20:26:43 +0100

    theme-provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
 import "../styles.css";
 
+import { ThemeProvider } from "next-themes";
 import React from "react";
 import App from "next/app";
 
 export default class TailwindApp extends App {
   render() {
     const { Component, pageProps } = this.props;
-    return <Component {...pageProps} />;
+    return (
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    );
   }
 }
